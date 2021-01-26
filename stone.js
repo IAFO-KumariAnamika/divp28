fa3db3c90d5bc183fdbfcff798711110b45edcde
IAFO-KumariAnamika <kumarianamika5798@gmail.com>
2021-01-26 22:54:14 +0530

    add

--- stone.js
+++ stone.js
@@ -9,15 +9,15 @@ class Stone{
  this.x=x;
  this.y=y;
  this.r=r;
- this.image=loadImage("images/stone.png");
- this.body=Bodies.circle(this.x,this.y,this.r,options);
+ this.image=loadImage("stone.png");
+ this.body=Bodies.circle(this.x,this.y,this.r/2,options);
  World.add(world,this.body);
 }
 display(){
 push();
 translate(this.body.position.x,this.body.position.y);
 imageMode(CENTER);
-this.image(this.image,0,0,this.r);
+image(this.image,0,0,this.r,this.r);
 pop();
 
 
